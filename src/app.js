d2b8b1d74f9dc90309b899da13a1b6868b8a4835
andrewbdesign <hello@andrewbdesign.com>
2018-10-27 07:45:36 +1100

    removed dummy expense data

--- src/app.js
+++ src/app.js
@@ -12,19 +12,6 @@ import './styles/styles.scss'
 
 const store = configureStore()
 
-// store.subscribe(() => {
-//     const state = store.getState()
-//     const visibleExpenses = getVisibleExpenses(state.expenses, state.filters)
-//     console.log(visibleExpenses)
-// })
-
-// console.log(store.getState())
-store.dispatch(addExpense({description: 'Water Bill', amount: 1500})) 
-store.dispatch(addExpense({description: 'Gas Bill', createdAt: 1000})) 
-store.dispatch(addExpense({description: 'Rent', amount: 109500})) 
-
-// store.dispatch(setTextFilter('bill'))
-
 const jsx = (    
     <Provider store={store}>
         <AppRouter />
